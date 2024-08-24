471ee23a8054405a87032cc8a7682d270aea91ab
Benjamin <ben.ngzw@gmail.com>
2024-08-24 14:57:34 +0800

    Add mongoose user schemas

---
+++ audio-files-backend/src/mongoose/schemas/user.ts
@@ -0,0 +1,29 @@
+import mongoose, { Schema, Model } from "mongoose";
+
+export interface User {
+  username: string;
+  password: string;
+  displayName: string;
+}
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: mongoose.Schema.Types.String,
+    required: true,
+  },
+  displayName: mongoose.Schema.Types.String,
+  isAdmin: {
+    type: mongoose.Schema.Types.Boolean,
+    default: false,
+  },
+});
+
+export const UserModel: Model<User> = mongoose.model<User, Model<User>>(
+  "User",
+  UserSchema
+);
