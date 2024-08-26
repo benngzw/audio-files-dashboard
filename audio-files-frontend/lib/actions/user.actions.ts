bf29ab9dc1fc4f4c5fb0bd6a71db524a425b46d5
Benjamin <ben.ngzw@gmail.com>
2024-08-27 03:08:08 +0800

    Clean up user actions

--- audio-files-frontend/lib/actions/user.actions.ts
+++ audio-files-frontend/lib/actions/user.actions.ts
@@ -15,28 +15,12 @@ export async function login(username: string, password: string) {
         withCredentials: true,
       }
     );
-    const backendCookie = response.headers["set-cookie"];
-    console.log(`backendCookie: ${backendCookie}`);
-    // if (backendCookie && backendCookie.length > 0) {
-    //   const [cookieNameValue, ...cookieAttributes] =
-    //     backendCookie[0].split("; ");
-    //   const [cookieName, cookieValue] = cookieNameValue.split("=");
-    // }
-    // console.log(response.data);
-
     const setCookieHeader = response.headers["set-cookie"];
-    console.log(setCookieHeader);
-
-    let cookiesString = "";
-
     if (setCookieHeader) {
-      cookiesString = setCookieHeader
-        .map((cookie: string) => cookie.split(";")[0])
-        .join("; ");
       setCookieHeader.forEach((cookieString: string) => {
         const [cookieNameValue, ...cookieAttributes] = cookieString.split("; ");
         const [cookieName, cookieValue] = cookieNameValue.split("=");
-        cookies().set("connect.sid", cookieValue, {
+        cookies().set(cookieName, cookieValue, {
           path: "/",
           ...Object.fromEntries(
             cookieAttributes.map((attr) => attr.split("="))
@@ -44,31 +28,13 @@ export async function login(username: string, password: string) {
         });
       });
     }
-
-    console.log(`cookiesString: ${cookiesString}`);
-    // console.log(`cookiesString: ${cookiesString}`);
-
-    // Use the cookies in another Axios call
-    // const anotherResponse = await axios.get(
-    //   "http://localhost:3000/auth/status",
-    //   {
-    //     withCredentials: true,
-    //     headers: {
-    //       Cookie: cookiesString,
-    //     },
-    //   }
-    // );
   } catch (error) {
     console.error(error);
   }
 }
 
 export async function getCurrentUser() {
-  console.log("getCurrentUser");
   const backendCookie = `connect.sid=${cookies().get("connect.sid")?.value}`;
-  console.log(`backendCookie: ${backendCookie}; `);
-  console.log(cookies().getAll());
-
   try {
     const anotherResponse = await axios.get(
       "http://localhost:3000/auth/status",
@@ -79,7 +45,6 @@ export async function getCurrentUser() {
         },
       }
     );
-    console.log(anotherResponse.data);
     return anotherResponse.data;
   } catch (error) {
     console.error(error);
