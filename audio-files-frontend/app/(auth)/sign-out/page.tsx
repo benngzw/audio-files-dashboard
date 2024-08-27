fe5e83aceaa8a0f28222e36c3cf9a57aa1467997
Benjamin <ben.ngzw@gmail.com>
2024-08-27 23:41:58 +0800

    Dockerize audio-files-frontend

--- audio-files-frontend/app/(auth)/sign-out/page.tsx
+++ audio-files-frontend/app/(auth)/sign-out/page.tsx
@@ -1,20 +1,14 @@
 "use client";
 
 import { Button } from '@/components/ui/button';
-import { useUser } from '@/providers/UserProvider';
-import { redirect } from 'next/navigation';
 import React from 'react'
 
 const SignOut = () => {
-  const { user } = useUser();
-  if (!user) redirect("/sign-in");
-
   const handleLogClick = async () => {
     // const anotherResponse = await axios.get("http://localhost:3000/auth/status", {
     //   withCredentials: true
     // })
     // console.log(anotherResponse.data);
-    console.log(user);
   };
 
   return (
